2f05068b044e574414136446da68ab880c9b691e
Gutemberg Batista da Silva <guter.silva@hotmail.com>
2026-03-26 13:42:55 -0300

    Criando projeto e classes

---
+++ gerador-escala-automatico/src/main/java/br/com/gutemberg/meuprojeto/Main.java
@@ -0,0 +1,10 @@
+package br.com.gutemberg.meuprojeto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Projeto Iniciado!");
+	}
+
+}
---
+++ gerador-escala-automatico/src/main/java/br/com/gutemberg/meuprojeto/model/Intervalo.java
@@ -0,0 +1,5 @@
+package br.com.gutemberg.meuprojeto.model;
+
+public class Intervalo {
+
+}
