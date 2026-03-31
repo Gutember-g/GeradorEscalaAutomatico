bd722fc8537a2c3ae19121e5460bc45d9070d6d6
Gutemberg Batista da Silva <guter.silva@hotmail.com>
2026-03-31 18:32:59 -0300

    criando os metodos da classe intervalo

--- gerador-escala-automatico/src/main/java/br/com/gutemberg/meuprojeto/model/Intervalo.java
+++ gerador-escala-automatico/src/main/java/br/com/gutemberg/meuprojeto/model/Intervalo.java
@@ -1,5 +1,21 @@
 package br.com.gutemberg.meuprojeto.model;
 
-public class Intervalo {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Intervalo {
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+	List<LocalDate> Dias;
+	
+	
+	public Intervalo(LocalDate dataInicio, LocalDate dataFim) {
+		
+	}
+	
+	
+	boolean contem(LocalDate data) {
+		boolean contem = data.isAfter(dataInicio) && data.isBefore(dataFim) || data.isEqual(data);
+		return true;
+	}
 }
